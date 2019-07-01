7dabe6c2c3a8555986f5514587126830d21b3e5e
“Elyse <elysemyers1@gmail.com>
2019-06-30 21:25:42 -0600

    Removes comments from file

--- main.js
+++ main.js
@@ -1,9 +1,5 @@
-// declaring a function called updateDogName
 function updateDogName() {
-//telling the interpreter that the default action should not be taken for this event  
   event.preventDefault();
-//declaring a variable called user input and using the method getElementById to target HTML input element with id "form-text"
   var userInput = document.getElementById("form-text");
-//using the method getElementbyId to update the innerHTML of the H1 with id of dog name with the value of userInput
   document.getElementById("dog-name").innerHTML = userInput.value;
 }
